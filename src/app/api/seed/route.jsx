da004fc9d09a41133ed3de1ef285ea4cda2c07e5
GAURJIsaheb <adityagaur086@gmail.com>
2025-03-24 19:19:00 +0530

    Fix line ending issues for node_modules and .next

--- src/app/api/seed/route.jsx
+++
@@ -1,7 +0,0 @@
-import { seedTransactions } from "@/Serveractions/seed";
-import { NextResponse } from "next/server";
-
-export async function GET(){
-    const result=await seedTransactions();
-    return NextResponse.json(result)
-}
--- src/lib/upstash.js
+++
@@ -1,9 +0,0 @@
-import { Ratelimit } from "@upstash/ratelimit";
-import { Redis } from "@upstash/redis";
-
-export const rateLimit=new Ratelimit({
-    redis:Redis.fromEnv(),
-    limiter:Ratelimit.slidingWindow(5,"3600 s"),
-    analytics:true,
-    timeout:10000,//10 seconds
-});
