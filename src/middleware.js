6ef2d89c1be2afdf2085920c8463de9a8fe73fca
GAURJIsaheb <adityagaur086@gmail.com>
2025-03-25 14:59:59 +0530

    solving CORS Headers problem

--- src/middleware.js
+++ src/middleware.js
@@ -28,7 +28,15 @@ const clerk= clerkMiddleware(async (auth, request) => {
     return NextResponse.redirect(`${baseUrl}/sign-in`);
 
   }
-})
+  const response = NextResponse.next();
+
+  // Add CORS headers
+  response.headers.set("Access-Control-Allow-Origin", "*");
+  response.headers.set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  response.headers.set("Access-Control-Allow-Headers", "Content-Type, Authorization");
+
+  return response;
+});
 
 export default createMiddleware(aj,clerk)
 export const config = {
